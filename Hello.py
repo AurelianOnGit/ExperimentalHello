89eb5c4cfe1437294d93e5c5b1ee65580af4fc70
MTTN <mhooethwin@gmail.com>
2024-04-15 20:10:29 +0700

    Made some small changes, do not judge me you peasant

--- Hello.py
+++ Hello.py
@@ -2,7 +2,15 @@ def main():
     Hello()
 
 def Hello():
-    Name = input("What's your name: ")
+    Name = checkString()
     print(f"Hello, {Name}")
+
+def checkString():
+    while True:
+         name = input("What's your name: ")
+         if len(name) > 10:
+            print("Your name is too long")
+         elif (len(name) == 0):
+            print("Your name cannot be blank")
 
 main()
